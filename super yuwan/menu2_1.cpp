0f2e6133d7b5a1e293dcb8a4cc949a77ec54d6f1
a5468 <a5468@YUYUAN>
2023-06-13 00:01:41 +0800

    添加项目文件。

---
+++ super yuwan/menu2_1.cpp
@@ -0,0 +1,107 @@
+//menu2_1.cpp
+#include <stdio.h>
+#include <iostream>
+void m2_1_1(void) {    //1. 数据分类——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C数据分类 \n\n  ");
+	printf("分为 整形 int ,实型 float，字符型 char\n");
+	printf("\t  int又分为 short、int、long \n");
+	printf(" 每种整形又分为 signed和unsigned \n");
+	printf("故整形细分有六种");
+	getchar();
+
+}
+void m2_1_2(void) {    //2. 数据类型的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C数据类型的定义 \n\n  ");
+	printf("数据类型的定义：\n");
+	printf("  1. 基本类型：int、float、char、double\n");
+	printf("  2. 构造类型：数组、结构体、共用体、枚举\n");
+	printf("  3. 指针类型：int *p;\n");
+	printf("  4. 空类型：void\n");
+	getchar();
+
+}
+void m2_1_3(void) {    //3. 整形变量的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C整形变量的定义 \n\n  ");
+	printf("整形变量的定义：\n");
+	printf("  1. short int a;\n");
+	printf("  2. int b;\n");
+	printf("  3. long int c;\n");
+	printf("  4. unsigned int d;\n");
+	printf("  5. signed int e;\n");
+	printf("  6. long long int f;\n");
+	getchar();
+
+}
+void m2_1_4(void) {    //4. 实型变量的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C实型变量的定义 \n\n  ");
+	printf("实型变量的定义：\n");
+	printf("  1. float a;\n");
+	printf("  2. double b;\n");
+	printf("  3. long double c;\n");
+	getchar();
+
+}
+void m2_1_5(void) {    //5. 字符型变量的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C字符型变量的定义 \n\n  ");
+	printf("字符型变量的定义：\n");
+	printf("  1. char a;\n");
+	printf("  2. char b[10];\n");
+	printf("  3. char *c;\n");
+	getchar();
+
+}
+void m2_1_6(void) {    //6. 常量的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C常量的定义 \n\n  ");
+	printf("常量的定义：\n");
+	printf("  1. 整形常量：int a=10;\n");
+	printf("  2. 实型常量：float b=3.14;\n");
+	printf("  3. 字符常量：char c='a';\n");
+	printf("  4. 字符串常量：char d[10]=\"abc\";\n");
+	printf("  5. 枚举常量：enum color{red,green,blue};\n");
+	printf("  6. 指针常量：int *p;\n");
+	printf("  7. 空常量：void;\n");
+	getchar();
+
+}
+void m2_1_7(void) {    //7. 变量的定义——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C变量的定义 \n\n  ");
+	printf("变量的定义：\n");
+	printf("  1. 整形变量：int a;\n");
+	printf("  2. 实型变量：float b;\n");
+	printf("  3. 字符变量：char c;\n");
+	printf("  4. 枚举变量：enum color{red,green,blue};\n");
+	printf("  5. 指针变量：int *p;\n");
+	getchar();
+
+}
+void m2_1_8(void) {    //8. 变量的初始化——肖洪生 
+
+	system("cls");     //调用系统命令，清屏
+	printf("\n\n    C变量的初始化 \n\n  ");
+	printf("变量的初始化：\n");
+	printf("  1. 整形变量：int a=10;\n");
+	printf("  2. 实型变量：float b=3.14;\n");
+	printf("  3. 字符变量：char c='a';\n");
+	printf("  4. 字符串变量：char d[10]=\"abc\";\n");
+	printf("  5. 枚举变量：enum color{red,green,blue};\n");
+	printf("  6. 指针变量：int *p;\n");
+	getchar();
+
+}
+`
+
+
